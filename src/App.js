d09202ded3839a4e48de88805c5dbc9f685399ca
AranyaSen <aranyasen62@gmail.com>
2025-04-13 22:03:27 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/myPortfolio" Component={LandingPage} />
+        <Route path="*" Component={NotFound}/>
       </Routes>
     </BrowserRouter>
   );
